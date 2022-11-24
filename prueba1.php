49387d7c2af7ace8360c06bcbf84f70427707f76
BryanAAnthony <115171642+BryanAAnthony@users.noreply.github.com>
2022-11-24 10:30:03 -0500

    avance

---
+++ prueba1.php
@@ -0,0 +1,19 @@
+<?php
+$a = 1;
+$b = 100;
+//$c =10;
+
+//if ($a > $b and $a > $c){
+    //echo $a. "Es mayor que".$a;
+//}else if ($b>$a and $b>$c){
+    //echo "el mayor es".$b;
+//}else{
+   // echo "el mayor es".$c;
+//}
+ while ($a <$b) {
+     echo $a."<br>";
+     $a++;
+ }
+
+
+?>
